ae61633bdf7dcad546bf8a1b25d2387e8b441b3e
Rupesh115 <rupeshrajbhar115@gmail.com>
2025-03-19 23:30:44 +0530

    Librozol Form

---
+++ src/api/librozol-form/controllers/librozol-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * librozol-form controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::librozol-form.librozol-form');
---
+++ src/api/librozol-form/services/librozol-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * librozol-form service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::librozol-form.librozol-form');
